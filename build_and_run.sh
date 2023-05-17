c11b437a151e510a301ab83fa02cec54ad5b2f17
ardocrat <ardocrat@proton.me>
2023-05-17 21:06:02 +0300

    build: update build script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,17 @@
-mode="release"
-export CPPFLAGS="-DMDB_USE_ROBUST=0" && export CFLAGS="-DMDB_USE_ROBUST=0" && cargo ndk -t arm64-v8a build --${mode}
+#!/bin/bash
+
+case $1 in
+  debug|release)
+    ;;
+  *)
+  echo "Usage: build_and_run.sh [type] where is type is 'debug' or 'release'" >&2
+  exit 1
+esac
+
+type=$1
+[[ ${type} == "release" ]] && release_param+=(--release)
+export CPPFLAGS="-DMDB_USE_ROBUST=0" && export CFLAGS="-DMDB_USE_ROBUST=0" && cargo ndk -t arm64-v8a build ${release_param[@]}
+
 if [ $? -eq 0 ]
 then
   yes | cp -f target/aarch64-linux-android/${mode}/libgrim_android.so app/src/main/jniLibs/arm64-v8a
