f9426287d500ea000f916d71cb6c918a5f3b5486
ardocrat <ardocrat@proton.me>
2024-10-25 20:03:57 +0300

    macos: release on darwin without zig, info.plist camera usage description and version update

--- macos/build_release.sh
+++ macos/build_release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-case $2 in
+case $1 in
   x86_64|arm|universal)
     ;;
   *)
-  echo "Usage: release_macos.sh [version] [platform]\n - platform: 'x86_64', 'arm', 'universal'" >&2
+  echo "Usage: release_macos.sh [platform] [version]\n - platform: 'x86_64', 'arm', 'universal'" >&2
   exit 1
 esac
 
@@ -24,17 +24,28 @@ cd ${BASEDIR}
 cd ..
 
 # Setup platform
-rustup target add x86_64-apple-darwin
-rustup target add aarch64-apple-darwin
+[[ $1 == "x86_64" ]] && arch+=(x86_64-apple-darwin)
+[[ $1 == "arm" ]] && arch+=(aarch64-apple-darwin)
 
-[[ $2 == "x86_64" ]] && arch+=(x86_64-apple-darwin)
-[[ $2 == "arm" ]] && arch+=(aarch64-apple-darwin)
-[[ $2 == "universal" ]]; arch+=(universal2-apple-darwin)
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  # Start release build on non-MacOS with zig linker, requires zig 0.12.1
+  rustup target add x86_64-apple-darwin
+  rustup target add aarch64-apple-darwin
+  [[ $1 == "universal" ]]; arch+=(universal2-apple-darwin)
+  cargo install cargo-zigbuild
+  cargo zigbuild --release --target ${arch}
+else
+  rustup target add ${arch}
+  if [[ $1 == "universal" ]]; then
+    cargo build --release --target x86_64-apple-darwin
+    cargo build --release --target aarch64-apple-darwin
+    lipo -create -output target/grim target/aarch64-apple-darwin/release/grim target/x86_64-apple-darwin/release/grim
+  else
+    cargo build --release --target ${arch}
+  fi
+fi
 
-# Start release build with zig linker, requires zig 0.12.1
-cargo install cargo-zigbuild
-cargo zigbuild --release --target ${arch}
-rm -rf .intentionally-empty-file.o
+rm -f .intentionally-empty-file.o
 
 yes | cp -rf target/${arch}/release/grim macos/Grim.app/Contents/MacOS
 
@@ -43,8 +54,7 @@ yes | cp -rf target/${arch}/release/grim macos/Grim.app/Contents/MacOS
 #rcodesign sign --pem-file cert.pem macos/Grim.app
 
 # Create release package
-FILE_NAME=grim-v$1-macos-$2.zip
-rm -rf target/${arch}/release/${FILE_NAME}
+FILE_NAME=grim-v$2-macos-$1.zip
 cd macos
 zip -r ${FILE_NAME} Grim.app
 mv ${FILE_NAME} ../target/${arch}/release
